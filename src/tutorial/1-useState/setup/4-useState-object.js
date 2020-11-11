ded80782b1f48255d10ffbe5d5a6849dabdaa616
Iyad <iyadhb@gmail.com>
2020-11-11 20:07:13 +0000

    testing commit

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 
 const UseStateObject = () => {
 const [person, setPerson] = useState({name: 'Peter' , age:51, message: 'Hey Man'});
+// (test)
 
 const changeMessage = () => {
   setPerson({...person, message:'Success'})
