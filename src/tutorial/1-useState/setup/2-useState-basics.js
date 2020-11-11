0271ac4c0c4619ec37de8d12c00084383c625f7b
Iyad <iyadhb@gmail.com>
2020-11-11 16:00:03 +0000

    useState Hook

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  const [text, setText] = useState("Random title");
+const handleClick = () => {
+  if(text === "Random title") {
+    setText('Iyad Barakat')
+  } else {
+    setText("Random title")
+  }
+  
+}
+
+  return (
+    <React.Fragment>
+    <h1>{text}</h1>
+    <button type="button" className="btn" onClick={handleClick}>Change Text</button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
