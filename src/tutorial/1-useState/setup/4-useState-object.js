96b9610dc4fcb3d4e83251266d18e9aeb4595ce6
Iyad <iyadhb@gmail.com>
2020-11-11 20:22:01 +0000

    mulltible useState

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -3,18 +3,23 @@ import React, { useState } from 'react';
 
 const UseStateObject = () => {
 const [person, setPerson] = useState({name: 'Peter' , age:51, message: 'Hey Man'});
-// (test)
+
+const [name, setName] = useState('Iyad');
+const [age, setAge] = useState(51);
+const [message, setMessage] = useState('React Superstar');
 
 const changeMessage = () => {
-  setPerson({...person, message:'Success'})
+  // setPerson({...person, message:'Success'})
+  setMessage('Soon will be')
+
 }
 
 
   return (
     <>
-    <h1>{person.name}</h1>
-    <h1>{person.age}</h1>
-    <h1>{person.message}</h1>
+    <h1>{name}</h1>
+    <h1>{age}</h1>
+    <h1>{message}</h1>
     <button className='btn' onClick={changeMessage }>change message</button>
     </>
   )
